fa353a5d2f40c079c1d73e51017b168445e4b66d
SilvermGit <c28javier.silverman-lord@afacademy.af.edu>
2026-09-04 16:04:47 -0600

    Lab08ActuallyDone

--- Lab08_InterfacingIO/LEDs.asm
+++ Lab08_InterfacingIO/LEDs.asm
@@ -21,7 +21,7 @@ P1OUT   .word 0x40004C02 ; replace 0 with the address
 
 ; Use this delay for Homework 8.
 ; You need to adjust this value in Lab8.
-DELAY   .word 1200000
+DELAY   .word 2000000
 
         ; global functions external function can access these functions.
         .global LED_Init
